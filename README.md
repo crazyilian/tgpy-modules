714c450ecab984efc7aaad856a3540d9d62fc6a3
Ilian Andrianov <ilian.and.04@gmail.com>
2023-03-04 06:48:23 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# tgpy-modules
+# TGPy modules made by crazyilian
+
+Some modules made for [TGpy](https://github.com/tm-a-t/TGPy).
+
---
+++ modules/mood.py
@@ -0,0 +1,108 @@
+"""
+    description: get most positive and negative messages in chat & get mood of one message
+    name: mood
+    needs_pip:
+      dostoevsky: dostoevsky
+    version: 0.2.0
+"""
+from dostoevsky.tokenization import RegexTokenizer
+from dostoevsky.models import FastTextSocialNetworkModel
+import dostoevsky.data
+import os
+from datetime import datetime
+from operator import itemgetter
+import telethon
+
+tokenizer = RegexTokenizer()
+model = FastTextSocialNetworkModel(tokenizer=tokenizer)
+
+
+def install_fasttext():
+    source, destination = dostoevsky.data.AVAILABLE_FILES['fasttext-social-network-model']
+    destination_path = os.path.join(dostoevsky.data.DATA_BASE_PATH, destination)
+    if not os.path.exists(destination_path):
+        downloader = dostoevsky.data.DataDownloader()
+        downloader.download(source=source, destination=destination)
+
+
+async def predict_and_get_top_results(ids, texts, title, msg):
+    results = model.predict(texts, k=2)
+    dict_pos = {}
+    dict_neg = {}
+    for id, res in zip(ids, results):
+        if 'positive' in res:
+            dict_pos[id] = res['positive']
+        if 'negative' in res:
+            dict_neg[id] = res['negative']
+    list_pos = reversed(sorted(dict_pos.items(), key=itemgetter(1)))[:5]
+    list_neg = reversed(sorted(dict_neg.items(), key=itemgetter(1)))[:5]
+    if msg.chat or isinstance(msg.chat, telethon.tl.types.User):
+        txt = f'TOP positive messages ' + title + ':'
+        for id, val in list_pos:
+            txt = txt + f'\nlink: t.me/c/{msg.chat.id}/{id}\nval: {val}'
+        await msg.reply(txt)
+        txt = f'TOP negative messages ' + title + ':'
+        for id, val in list_neg:
+            txt = txt + f'\nlink: t.me/c/{msg.chat.id}/{id}\nval: {val}'
+        await msg.reply(txt)
+    else:
+        for id, val in list_neg:
+            await msg.respond(f'positive: {val}', reply_to=id)
+        for id, val in list_pos:
+            await msg.respond(f'negative: {val}', reply_to=id)
+
+
+def extract_text(msg):
+    try:
+        text = msg.raw_text
+    except:
+        return None
+    if not isinstance(text, str) or len(text) < 5:
+        return None
+    return text
+
+
+async def topmoodcnt(topcnt_=1000):
+    ct = datetime.now().astimezone()
+    texts = []
+    ids = []
+    topcnt = 0
+    async for mess in client.iter_messages(ctx.msg.chat_id):
+        if topcnt + 1 > topcnt_:
+            break
+        topcnt += 1
+        text = extract_text(mess)
+        if text:
+            texts.append(mess.text)
+            ids.append(mess.id)
+    await predict_and_get_top_results(ids, texts, f'among the last {topcnt}', ctx.msg)
+    return 'Done'
+
+
+async def topmood(days=1):
+    msg = ctx.msg
+    ct = datetime.now().astimezone()
+    texts = []
+    ids = []
+    async for mess in client.iter_messages(msg.chat_id):
+        if (ct - mess.date).days >= days:
+            break
+        text = extract_text(mess)
+        if text:
+            texts.append(mess.text)
+            ids.append(mess.id)
+    await predict_and_get_top_results(ids, texts, 'of the day', msg)
+    return 'Done'
+
+
+async def sentiment():
+    msg = await ctx.msg.get_reply_message()
+    txt = msg.text
+    res = model.predict([txt])[0]
+    return '\n' + '\n'.join(map(lambda el: f'{el[0]}: {round(el[1], 7)}',
+                                filter(lambda el: el[1] > 0.0001, sorted(res.items(), key=lambda el: -el[1]))))
+
+
+install_fasttext()
+
+__all__ = ['sentiment', 'topmood', 'topmoodcnt']
