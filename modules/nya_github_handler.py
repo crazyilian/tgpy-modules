d84ee1b30a3cd9cddf1d0f92e01d124175935afa
Ilian Andrianov <ilian.and.04@gmail.com>
2023-03-05 14:13:31 +0300

    [feat]: gitlab and github nya source handlers

---
+++ modules/nya_github_handler.py
@@ -0,0 +1,37 @@
+"""
+    name: nya_github_handler
+    description: github source handler for Nya
+    needs:
+      nya: 0.32.6
+    needs_pip:
+      github: pygithub
+    version: 0.0.0
+"""
+from urllib.parse import urlparse
+import tgpy.api
+import github
+
+
+class GithubHandler:
+    def __init__(self):
+        self.gh = github.Github(self.get_token())
+
+    def handler(self, src):
+        path = urlparse(src).path.strip('/').split('/')
+        repo = path[0] + '/' + path[1]
+        branch = path[3]
+        file = '/'.join(path[4:])
+        return self.gh.get_repo(repo).get_contents(file, branch).decoded_content.decode('utf-8')
+
+    def get_token(self):
+        return tgpy.api.config.get(f'nya_github_handler.token', None)
+
+    def set_token(self, token=None):
+        self.gh = github.Github(token)
+        tgpy.api.config.set(f'nya_github_handler.token', token)
+
+
+nya.github_handler = GithubHandler()
+nya.add_source_handler(('https', 'github.com'), nya.github_handler.handler)
+
+__all__ = []
---
+++ modules/nya_gitlab_handler.py
@@ -0,0 +1,50 @@
+"""
+    name: nya_gitlab_handler
+    description: gitlab source handler for Nya
+    needs:
+      nya: 0.32.6
+    needs_pip:
+      gitlab: python-gitlab
+    version: 0.0.0
+"""
+from urllib.parse import urlparse
+import tgpy.api
+import gitlab
+
+
+class GitlabHandler:
+    def __init__(self):
+        pass
+
+    def handler(self, src):
+        parsed = urlparse(src)
+        domain = parsed.scheme + '://' + parsed.netloc
+        path = parsed.path.strip('/').split('/')
+        repo = path[0] + '/' + path[1]
+        branch = path[4]
+        file = '/'.join(path[5:])
+        gl = gitlab.Gitlab(url=domain, private_token=self.get_domain_token(domain))
+        return gl.projects.get(repo).files.get(file, branch).decode().decode('utf-8')
+
+    @staticmethod
+    def stringify_domain(domain):
+        domain = domain.strip('/')
+        parsed = urlparse(domain)
+        if not parsed.scheme:
+            parsed = urlparse('https://' + domain)
+        domain = parsed.scheme + '://' + parsed.netloc
+        return domain.replace('.', '___')
+
+    def get_domain_token(self, domain='https://gitlab.com'):
+        domain = self.stringify_domain(domain)
+        return tgpy.api.config.get(f'nya_gitlab_handler.tokens.{domain}', None)
+
+    def set_domain_token(self, domain='https://gitlab.com', token=None):
+        domain = self.stringify_domain(domain)
+        tgpy.api.config.set(f'nya_gitlab_handler.tokens.{domain}', token)
+
+
+nya.gitlab_handler = GitlabHandler()
+nya.add_source_handler(('https', 'gitlab.com'), nya.gitlab_handler.handler)
+
+__all__ = []
