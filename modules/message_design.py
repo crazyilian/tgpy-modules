b1b7e107d403e1c3a85cd830f9831238a95dd993
Ilian Andrianov <ilian.and.04@gmail.com>
2023-03-06 04:06:26 +0300

    [feat]: set_tgpy_prefix function

--- modules/message_design.py
+++ modules/message_design.py
@@ -1,10 +1,18 @@
 """
     description: replace default "TGPy>" prefix
     name: message_design
-    version: 0.0.0
+    version: 1.0.0
 """
 import tgpy._core.message_design as message_design
+import tgpy.api
 
-message_design.TITLE = "$>"
+DEFAULT_TITLE = message_design.TITLE
+message_design.TITLE = tgpy.api.config.get('message_design.prefix', DEFAULT_TITLE)
 
-__all__ = []
+
+def set_tgpy_prefix(prefix=DEFAULT_TITLE):
+    message_design.TITLE = prefix
+    tgpy.api.config.set('message_design.prefix', prefix)
+
+
+__all__ = [set_tgpy_prefix]
