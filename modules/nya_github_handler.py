ceae97b4fec015c4b08f93675ebed9fedf344b6a
Ilian Andrianov <ilian.and.04@gmail.com>
2023-03-09 01:54:35 +0300

    [feat]: use raw

--- modules/nya_github_handler.py
+++ modules/nya_github_handler.py
@@ -4,40 +4,38 @@
     needs:
       nya: 0.32.6
     needs_pip:
-      github: pygithub
-    version: 0.1.2
+      aiohttp: aiohttp
+    version: 0.3.0
 """
 from urllib.parse import urlparse
 import tgpy.api
-import github
-from github.ContentFile import ContentFile
-from github.GithubObject import NotSet
-import os.path
-from telethon.tl.types import MessageEntityCode
+import aiohttp
 
 
 class GithubHandler:
     """
-        It is STRONGLY RECOMMENDED to set your github token, otherwise you have limit only 60 requests per hour.
-        For authenticated users there are 5000 requests per hour limit.
+        It is obligatory to set your github token if you want to access private repos.
     """
 
     def __init__(self):
-        self.gh = github.Github(self.get_token())
+        self.session = None
+        self.__create_session(self.get_token())
 
-    @staticmethod
-    def parse_src(src):
-        """Split repo, branch and file path from url"""
-        path = urlparse(src).path.strip('/').split('/')
-        repo = path[0] + '/' + path[1]
-        branch = path[3] if len(path) >= 3 else NotSet
-        file = '/'.join(path[4:])
-        return repo, branch, file
+    def __create_session(self, token):
+        if self.session:
+            self.session.close()
+        self.session = aiohttp.ClientSession(headers={'Authorization': f'Token {token}'} if token else {})
 
-    async def handler(self, src):
+    async def _handler(self, src):
         """Get file content by url"""
-        repo, branch, file = self.parse_src(src)
-        return self.gh.get_repo(repo).get_contents(file, branch).decoded_content.decode('utf-8')
+        path = urlparse(src).path.strip('/').split('/')
+        path.pop(2)
+        download_url = f'https://raw.githubusercontent.com/' + '/'.join(path)
+        return await self._raw_handler(download_url)
+
+    async def _raw_handler(self, src):
+        """Get file content by raw url"""
+        return await (await self.session.get(src)).text()
 
     def get_token(self):
         """Get you current access token"""
@@ -45,39 +43,12 @@ class GithubHandler:
 
     def set_token(self, token=None):
         """Set access token (https://github.com/settings/tokens)"""
-        self.gh = github.Github(token)
+        self.__create_session(token)
         tgpy.api.config.set(f'nya_github_handler.token', token)
-
-    async def share_urls(self, src, recursive=True, extensions=('.py',), use_raw=False):
-        """Share url of file or every file in directory (nya registry format)"""
-        repo_name, branch, dir_path = self.parse_src(src)
-        repo = self.gh.get_repo(repo_name)
-        contents = repo.get_contents(dir_path, branch)
-        if isinstance(contents, ContentFile):
-            contents = [contents]
-        registry = {}
-        while contents:
-            content = contents.pop(0)
-            if content.type == "file":
-                extension = os.path.splitext(content.path)[1]
-                if extension in extensions:
-                    try:
-                        module_name = nya._Nya__parse(content.decoded_content.decode('utf-8'))[0]
-                        if use_raw or module_name in ('nya_github_handler', 'nya_gitlab_handler'):
-                            url = content.download_url
-                        else:
-                            url = content.html_url
-                        registry[module_name] = url
-                    except:
-                        print(f"ERROR: Not a module: {content.html_url}")
-            elif content.type == "dir":
-                if recursive:
-                    contents.extend(repo.get_contents(content.path))
-        ans = '"""\n    sources:\n' + "\n".join([f'        {name}: "{registry[name]}"' for name in registry]) + '\n"""'
-        await ctx.msg.respond(ans, formatting_entities=[MessageEntityCode(0, len(ans.encode('utf-16-le')) // 2)])
 
 
 nya.github_handler = GithubHandler()
-nya.add_source_handler(('https', 'github.com'), nya.github_handler.handler)
+nya.add_source_handler(('https', 'github.com'), nya.github_handler._handler)
+nya.add_source_handler(('https', 'raw.githubusercontent.com'), nya.github_handler._raw_handler)
 
 __all__ = []
