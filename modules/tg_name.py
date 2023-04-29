6e4172a3689c123d6fb765b534f6b4d05ddcfb4b
Ilian Andrianov <ilian.and.04@gmail.com>
2023-04-29 03:05:22 +0300

    [fix]: tg_name: chat title

--- modules/tg_name.py
+++ modules/tg_name.py
@@ -1,13 +1,13 @@
 """
     description: get username/first name/title of telethon entity object
     name: tg_name
-    version: 0.0.0
+    version: 0.0.1
 """
 import telethon.tl.types
 
 
 def get_name(user, try_username=True):
-    if isinstance(user, telethon.tl.types.Channel):
+    if isinstance(user, telethon.tl.types.Channel) or isinstance(user, telethon.tl.types.Chat):
         return f'"{user.title}"'
     if try_username and user.username:
         return '@' + user.username
