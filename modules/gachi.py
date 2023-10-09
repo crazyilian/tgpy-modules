6995cb0b7c587ba7995b69cfdca8daff323b608b
Ilian Andrianov <ilian.and.04@gmail.com>
2023-10-09 03:47:27 +0300

    [fix]: gachi empty message

--- modules/gachi.py
+++ modules/gachi.py
@@ -2,9 +2,12 @@
     name: gachi
     needs:
         dot_hook: 0.0.1
+    version: 0.0.1
 """
 
 
 @dot_hook('gachi')
-async def gachi(text, message, is_edit):
+async def gachi(text="", message=None, is_edit=None):
+    if message is None:
+        return
     await message.edit("♂️" + text + "♂️")
